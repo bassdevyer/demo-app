77b529392b4daf6ec69ef66a3a25a80e3ad7852e
Miguel Torres <miguel@alturasoluciones.com>
2018-12-31 16:03:02 -0500

    React Router - Nested Routing

--- src/App.js
+++ src/App.js
@@ -2,35 +2,61 @@ import React, { Component } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-const Index = () => <h2>Home</h2>
-const About = () => <h2>About</h2>
-const Users = () => <h2>Users</h2>
+/**
+ * This example shows how nested routing works. The route /topics loads the 
+ * Topics component, which renders any further <Route>'s conditionally on the 
+ * paths :id value.import 
+ */
 
 class App extends Component {
   render = () => (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about/'>About</Link>
-            </li>
-            <li>
-              <Link to='/users/'>Users</Link>
-            </li>
-          </ul>
-        </nav>
+        <Header/>
 
-        <Route path='/' exact component={Index}/>
-        <Route path='/about/' component={About}/>
-        <Route path='/users/' component={Users}/>
+        <Route path='/' exact component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/topics' component={Topics}/>
       </div>
     </Router>
   )
 }
+
+const Home = () => <h2>Home</h2>
+const About = () => <h2>About</h2>
+const Topic = ({match}) => <h3>Requested param: {match.params.id}</h3>
+
+const Topics = ({match}) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Topic}/>
+    <Route exact path={`${match.path}`}
+           render={() => <h3>Please select a topic.</h3>}/>
+  </div>
+)
+
+const Header = () => (
+  <ul>
+    <li>
+      <Link to='/'>Home</Link>
+    </li>
+    <li>
+      <Link to='/about'>About</Link>
+    </li>
+    <li>
+      <Link to='/topics'>Topics</Link>
+    </li>
+  </ul>
+)
 
 export default App
 
